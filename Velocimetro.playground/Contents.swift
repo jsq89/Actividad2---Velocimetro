7d416b5f72c36bd3a8477a0bde79245c1351375d
Jesus <jesussanchez@jesuss-mac-pro.local>
2016-06-25 16:31:33 +0200

    Initial Commit

---
+++ Velocimetro.playground/Contents.swift
@@ -0,0 +1,58 @@
+// Actividad 2 - Velocimetro
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init (velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+Velocidades.VelocidadAlta
+
+class Auto {
+    var velocidad : Velocidades
+    
+    init() {
+        self.velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String){
+        
+        let velocidadActual : Int = self.velocidad.rawValue
+        let velocidadActualEnCadena : String
+        switch (self.velocidad.hashValue){
+        case 0:
+            velocidadActualEnCadena = "Apagado"
+            self.velocidad = Velocidades.VelocidadBaja
+        case 1:
+            velocidadActualEnCadena = "Velocidad baja"
+            self.velocidad = Velocidades.VelocidadMedia
+        case 2:
+            velocidadActualEnCadena = "Velocidad media"
+            self.velocidad = Velocidades.VelocidadAlta
+        case 3:
+            velocidadActualEnCadena = "Velocidad alta"
+            self.velocidad = Velocidades.VelocidadMedia
+        default:
+            // En caso de error pondremos apagado el velocimetro
+            velocidadActualEnCadena = "Apagado"
+            self.velocidad = Velocidades.Apagado
+        }
+        return (velocidadActual, velocidadActualEnCadena)
+    }
+
+}
+
+
+var auto : Auto = Auto()
+
+var velocidadActual : Int
+var velocidadActualEnCadena : String
+
+for i in 1...20 {
+    (velocidadActual,velocidadActualEnCadena)=auto.cambioDeVelocidad()
+    print("\(velocidadActual) , \(velocidadActualEnCadena)")
+}
